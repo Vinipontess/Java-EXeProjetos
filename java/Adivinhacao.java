489083ba7cf87240f5e999e160c2631fb56db715
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2024-02-27 01:10:14 -0300

    Add files via upload

---
+++ java/Adivinhacao.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Adivinhacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroAleatorio = (int) (Math.random() * 11);
+        int tentativas = 0;
+
+        System.out.println("Bem-vindo ao jogo de adivinhação!");
+        System.out.println("Tente adivinhar o número entre 0 e 10.");
+
+        while (true) {
+            System.out.print("Digite um número: ");
+            int palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite == numeroAleatorio) {
+                System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas.");
+                break;
+            } else if (palpite < numeroAleatorio) {
+                System.out.println("Tente um número maior.");
+            } else {
+                System.out.println("Tente um número menor.");
+            }
+        }
+
+        scanner.close();
+    }
+}
