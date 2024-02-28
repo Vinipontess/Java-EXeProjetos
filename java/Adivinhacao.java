cffe236913208fbf1498ca38a1867c4824f6e40c
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2024-02-28 05:07:25 -0300

    Update Adivinhacao.java

--- java/Adivinhacao.java
+++ java/Adivinhacao.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-
 class Adivinhacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numeroAleatorio = (int) (Math.random() * 11);
         int tentativas = 0;
 
-        System.out.println("Bem-vindo ao jogo de adivinhação!");
-        System.out.println("Tente adivinhar o número entre 0 e 10.");
+        System.out.println("Bem-vindo ao jogo de adivinhaÃ§Ã£o!");
+        System.out.println("Tente adivinhar o nÃºmero entre 0 e 10.");
 
         while (true) {
-            System.out.print("Digite um número: ");
+            System.out.print("Digite um nÃºmero: ");
             int palpite = scanner.nextInt();
             tentativas++;
 
             if (palpite == numeroAleatorio) {
-                System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas.");
+                System.out.println("ParabÃ©ns! VocÃª acertou em " + tentativas + " tentativas.");
                 break;
             } else if (palpite < numeroAleatorio) {
-                System.out.println("Tente um número maior.");
+                System.out.println("Tente um nÃºmero maior.");
             } else {
-                System.out.println("Tente um número menor.");
+                System.out.println("Tente um nÃºmero menor.");
             }
         }
 
