11f63f66a1f7dfe7b67c7c428bf1d65e671cf8ba
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2024-03-10 11:24:47 -0300

    Update TP01EX01.java

--- TP01/TP01EX01.java
+++ TP01/TP01EX01.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 class TP01EX01{
 	public static void main(String[] args){
 
-		//1 - Entrar via teclado com a base e a altura de um ret‚ngulo, calcular e exibir sua
-·rea.		
+		//1 - Entrar via teclado com a base e a altura de um ret√¢ngulo, calcular e exibir sua √°rea.		
 
 		int altura;
 		int base;
 
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Calcular a ·rea de um ret‚ngulo (digite o tamanho em centÌmetros)");
+		System.out.println("Calcular a √°rea de um ret√¢ngulo (digite o tamanho em cent√≠metros)");
 		System.out.println("");
-		System.out.println("Digite a altura do seu ret‚ngulo");
+		System.out.println("Digite a altura do seu ret√¢ngulo");
 		altura = scan.nextInt();
-		System.out.println("Perfeito! Agora digite a base do seu ret‚ngulo ");
+		System.out.println("Perfeito! Agora digite a base do seu ret√¢ngulo ");
 		base = scan.nextInt();
 		int area = altura*base;
-		System.out.println("A ·rea do seu ret‚ngulo È de " + area + "cm");
+		System.out.println("A √°rea do seu ret√¢ngulo √© de " + area + "cm");
 		
 	}
 }
 
-// VinÌcius Pontes e Eduardo Barbosa
+// Vin√≠cius Pontes e Eduardo Barbosa
