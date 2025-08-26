2360890b32d62b32e62cf129de674b701ab87b7c
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2025-08-26 19:56:00 -0300

    Create Polimorfismo.java
    Polimorfismo.java incluindo na pasta

---
+++ java/Polimorfismo.java
@@ -0,0 +1,104 @@
+
+public class Polimorfismo
+{   
+    public static void imprimirSalario(Funcionario x){
+        System.out.println("Seu salário contando com o bônus é R$" + x.calcularSalario());
+    }
+
+	public static void main(String[] args) {
+		Gerente gerenteComercial = new Gerente("Vinicius", 10000, "Comercial");
+	    System.out.println(gerenteComercial.getNome() + " " + gerenteComercial.getSalario() + " " + gerenteComercial.getDepartamento());
+	    gerenteComercial.aumentarSalario(10);
+        imprimirSalario(gerenteComercial);
+        Programador programadorJava = new Programador("Vinicius", 8000, "Java");
+        programadorJava.setLinguagem("Python");
+        imprimirSalario(programadorJava);
+	}
+}
+
+abstract class Funcionario{
+    private String nome;
+    private double salario;
+    
+    //construtor
+    public Funcionario(String nome, double salario){
+        this.nome = nome;
+        this.salario = salario;
+    }
+    
+    //getters
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public double getSalario(){
+        return this.salario;
+    }
+    
+    //Setters
+    public void setNome(String novoNome){
+        this.nome = novoNome;
+    }
+    
+    public void setSalario(double salario){
+        System.out.println("O salário não pode ser alterado desta forma, solicite ao RH");
+    }
+    
+    //Métodos
+    void aumentarSalario(double porcentagem){
+        if(porcentagem > 0){
+            salario += (porcentagem/100)*salario;
+            System.out.println("O salário aumentou em " + porcentagem + "% seu novo salário é R$" + salario);
+        }else{
+            System.out.println("Porcentagem inválida");
+        }
+    }
+
+    abstract double calcularSalario();
+
+} //fim classe funcionario
+
+class Gerente extends Funcionario{
+    private String departamento;
+    
+    //construtor
+    public Gerente(String nome, double salario, String departamento){
+        super(nome, salario);
+        this.departamento = departamento;
+    }
+    
+    public String getDepartamento(){
+        return this.departamento;
+    }
+    
+    public void setDepartamento(String novoDepartamento){
+        System.out.println("Para migrar ao " + novoDepartamento + " será necessário solicitar ao RH");
+    }
+    @Override
+    public double calcularSalario(){
+        double salarioBase = getSalario();
+        return salarioBase + (salarioBase * 0.2);}
+} 
+
+class Programador extends Funcionario{
+    private String linguagem;
+
+    public Programador(String nome, double salario, String linguagem){
+        super(nome, salario);
+        this.linguagem = linguagem;
+    }
+
+    public String getLinguagem(){
+        return this.linguagem;
+    }
+
+    public void setLinguagem(String novaLinguagem){
+        System.out.println("Para migrar a " + novaLinguagem + " será necessário solicitar ao RH, pois terá de haver um cargo disponível");
+    }
+
+    @Override
+    public double calcularSalario(){
+        double salarioBase = getSalario();
+        return salarioBase + (salarioBase * 0.1);
+}}
+
